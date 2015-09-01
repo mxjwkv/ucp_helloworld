5092bb9ccafb3df435c4ce1bee7b6db667af0449
mxjwkv <mxjwkv@gmail.com>
2015-09-01 15:25:26 -0700

    Add lib calls

--- lin_build.sh
+++ lin_build.sh
@@ -1,2 +1,2 @@
 #lin_build.sh 02-AUG-2012
-g++ -o ucp_helloworld.x ucp_helloworld.cpp #-L../liblin -lclass_lin32 -L../liblin -lfunc_lin32
+g++ -o ucp_helloworld.x ucp_helloworld.cpp -L../lib -lclass -L../lib -lfunc
--- ucp_helloworld.cpp
+++ ucp_helloworld.cpp
@@ -3,11 +3,11 @@
 //     Created: 21-AUG-2015
 // Last Update: 01-SEP-2015
 
-#define VER 1
+#define VER 2
 
 #include "stdio.h"
-//#include "../wlp_libfunc/libfunc.h"
-//#include "../wlp_libclass/libclass.h"
+#include "../ulp_libfunc/libfunc.h"
+#include "../ulp_libclass/libclass.h"
 
 /*
 #ifdef _DEBUG
@@ -21,17 +21,17 @@
 
 int main(int argc, char* argv[])
 {
-/*
+
     x x1(100, 200); // create object x1
 
-	printf("ucp_helloworld - Ver %d (%s) Build %d\n", VER, gszBuildType, BUILD_NUM);
+    //printf("ucp_helloworld - Ver %d (%s) Build %d\n", VER, gszBuildType, BUILD_NUM);
 
     printf("wlp_libfunc - lib_test_incr_num(101) = %d\n", lib_test_incr_num(101));
 
     x1.setb(222);
     printf("wlp_libclass - x.display():\n");
     x1.display();
-**/
+
     printf("Press ENTER to exit . . .");
     getchar();
 
