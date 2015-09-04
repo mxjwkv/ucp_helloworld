2d039ca2c56d637443c5d8afaaf53133c4f32d1b
mxjwkv <mxjwkv@gmail.com>
2015-09-03 17:18:26 -0700

    Add build date and time

--- lin_build.sh
+++ lin_build.sh
@@ -1,2 +1,5 @@
-#lin_build.sh 02-AUG-2012
+# lin_build.sh
+#     Created: 02-AUG-2012
+# Last Update: 03-SEP-2015
+../../ucx_incrbuildnum.out __build_num_release -L
 g++ -o ucp_helloworld.out ucp_helloworld.cpp -L../lib -lulp_libclass -L../lib -lulp_libfunc
